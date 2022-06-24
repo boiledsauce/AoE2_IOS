6d3ff70deae1ba9b1eaba437c7ba6d9bbd2fea5e
brwi26 <williambranth96@gmail.com>
2022-06-24 16:23:43 +0200

    Initial Commit

---
+++ AoE2_IOS/AoE2_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AoE2_IOSApp.swift
+//  AoE2_IOS
+//
+//  Created by William Branth on 2022-06-24.
+//
+
+import SwiftUI
+
+@main
+struct AoE2_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AoE2_IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AoE2_IOS
+//
+//  Created by William Branth on 2022-06-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
